8224c62aed2bf0872c064ecdab507b1a63fe44c7
sqa <2947764906@qq.com>
2020-02-11 18:09:11 +0800

    首页更新

---
+++ src/network/request.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+
+export function request(config) {
+  // 1.创建axios的实例
+  const instance = axios.create({
+    baseURL: 'http://123.207.32.32:8000/api/hy',
+    timeout: 5000
+  })
+
+  instance.interceptors.request.use(config => {
+    return config
+  }, err => {
+    // console.log(err);
+  })
+
+  // 2.2.响应拦截
+  instance.interceptors.response.use(res => {
+    // console.log(res);
+    return res.data
+  }, err => {
+    console.log(err);
+  })
+
+  return instance(config)
+}
+
---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    configureWebpack:{
+        resolve:{
+            alias:{
+                'assets':'@/assets',
+                'common':'@/common',
+                'network':'@/network',
+                'router':'@/router',
+                'views':'@/views',
+                'components':'@/components',
+
+            }
+        }
+    }
+}//配置别名
